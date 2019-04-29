6668e1df0289a5b111c4286cd76a4c14af51b64c
PavloMenshykh <pavlo.menshykh@gmail.com>
2019-04-29 23:14:52 +0300

    refined blue

--- rgb into r g b outputs.py
+++ rgb into r g b outputs.py
@@ -2,7 +2,7 @@ color, red, green, blue = rgb.split(",")
 
 R = red[3:]
 G = green[3:]
-B = blue[3:len(blue)-1]
+B = blue[3:-1]
 
 #rgb is input string, looksd like Color [A=255, R=198, G=216, B=198]
 #R, G, B are aoutput strings
